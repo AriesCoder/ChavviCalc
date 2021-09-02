fb42c44e26e4d26ed0bdccd4c6f4642b88950d2a
Trang Lam <ms.tranglam@gmail.com>
2021-09-01 23:04:06 -0700

    got the command q to work and displayed the values of A and B in the menu

--- calculator/src/main/java/com/calcassignment/App.java
+++ calculator/src/main/java/com/calcassignment/App.java
@@ -8,25 +8,37 @@ import java.util.Scanner;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        Scanner input = new Scanner(System.in);
+    public static void main( String[] args ){
 
-        String[] arr = {"a", "b", "+", "-", "*", "/", "c", "q"};
+        //String[] arr = {"a", "b", "+", "-", "*", "/", "c", "q"};
+        float numberA = 0;
+        float numberB = 0;
         
-        addValue(0, 0);
-        String command = input.nextLine();
+        addValue(numberA, numberB);
+        
+        while (true){
+            Scanner input = new Scanner(System.in);
+            String command = input.nextLine();
 
-    }
+            //quit the program
+            if (command.equals("q")) {
+                System.out.println("Thank for using Chavvi Calc");
+                break;
+            }else{
+                addValue(numberA, numberB);
+            }
+        }
+    }        
     
 
     private static void addValue(float a, float b){
-        String str = String.format("A = %.3f            B = %.3f", a, b);
+       
+        String value = String.format("A = %.3f            B = %.3f", a, b);
 
         System.out.println("----------------------------------------");
         System.out.println("Chavvi Calc");
         System.out.println("----------------------------------------");
-        System.out.println(str);
+        System.out.println(value);
         System.out.println("----------------------------------------");
         System.out.println("a      Enter a value for A");
         System.out.println("b      Enter a value for B");
@@ -38,7 +50,8 @@ public class App
         System.out.println("q      Quit");
         System.out.println("----------------------------------------");
         System.out.printf("Enter a command: ");
+        }
     }
 
     
-}
+
